6fc57d3718033cbb53d3ddb7e0603930d79e8eff
Hakuo <94927854+StancaRadu@users.noreply.github.com>
2025-10-30 06:37:48 +0200

    test2

--- install/00-pre-install.sh
+++ install/00-pre-install.sh
@@ -1,1 +1,6 @@
-echo "Works"
+echo
+echo "Available disk devices:"
+echo "-----------------------"
+lsblk -dn -o NAME,MODEL,SIZE,TYPE | awk '$4=="disk" { printf("/dev/%-8s  %-20s  %6s\n",$1,$2,$3) }'
+echo "-----------------------"
+echo
